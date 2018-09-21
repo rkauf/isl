e760615fddc90e0c487f073177b8d62deedf861d
rkauf <ryankaufman67@gmail.com>
2018-09-21 10:34:08 -0400

    stated ch4

---
+++ munge/ch4.R
@@ -0,0 +1,15 @@
+#' # Chapter 4 Lab
+
+#+ setup
+knitr::opts_chunk$set(dpi = 200, fig.width = 8, fig.height = 5, message = F, warning = F)
+
+library(MASS)
+library(ISLR)
+library(tidyverse)
+library(tidymodels)
+library(expappr)
+library(modelr)
+
+options(scipen=999)
+
+#ezspin_pt(file_name = "ch4", project_directory = ".", file_folder = "munge", keep_html = F)
